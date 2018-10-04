079a91bc9dc04ed3d930152a01279584417c7bbf
Molly Huerster <mollyhuerster@squareup.com>
2018-10-04 16:18:19 -0700

    [STEP 5] Correct provider states
    https://github.com/DiUS/pact-workshop-ruby-v2#step-5---correct-provider-states

--- provider/spec/pact_helper.rb
+++ provider/spec/pact_helper.rb
@@ -5,3 +5,13 @@ Pact.service_provider 'Our Provider' do
     pact_uri 'spec/pacts/our_consumer-our_provider.json'
   end
 end
+
+Pact.provider_states_for 'Our Consumer' do
+
+  provider_state 'data count is > 0' do
+    set_up do
+      # Your set up code goes here
+    end
+  end
+
+end
